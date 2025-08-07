32b605f04433174c46ecbf987850714cbb111c67
Init commmit <fbasil1993@gmail.com>
2025-08-07 19:26:23 +0300

    обавлен прелоадер и правки позиционирования

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,12 @@ import './styles/normalize.css'
 import './styles/global.css';
 import App from './App.tsx'
 
+const initialPreloader = document.getElementById('initial-preloader')
+if (initialPreloader) {
+    initialPreloader.remove()
+}
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
